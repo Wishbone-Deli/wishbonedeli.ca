0fb65b15513c0b1ebafe513afa071f973d83eb2b
Kyu Shim <ks1771@naver.com>
2019-10-14 17:49:05 -0400

    add testing boilerplate

---
+++ src/__tests__/components/Home.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Home from "../../components/Home";
+import { shallow, ShallowWrapper } from "enzyme";
+
+describe("<Home />", () => {
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+
+  it("displays a greeting message", () => {
+    expect(wrapper.text()).toMatch(/Hello world!/);
+  });
+});
